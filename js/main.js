746704420b78eed6492e25b671a16fb3684b3a8a
sdkamanorg-source <sdkamanorg@gmail.com>
2026-04-07 18:40:49 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,3 +8,20 @@ function buy(item) {
   document.getElementById("modal").classList.remove("hidden");
   document.getElementById("modal-title").innerText = item;
 }
+
+function closeModal() {
+  document.getElementById("modal").classList.add("hidden");
+}
+
+function payNow() {
+  window.open("https://your-payment-link.com", "_blank");
+}
+
+function paymentDone() {
+  document.getElementById("payment-msg").innerText =
+    "✅ Make ticket in Discord!";
+}
+
+function openDiscord() {
+  window.open("https://discord.gg/HDpkv4N4", "_blank");
+}
